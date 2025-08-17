ccc17b646d94cc04c07abc31d4c6bff95056b0c8
self-raj <rajbhadurs809@gmail.com>
2025-08-17 10:10:48 +0530

    origin commit

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -29,7 +29,7 @@ export default function Contact() {
     setLoading(true); // Loader 
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://clean-backend-3.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
